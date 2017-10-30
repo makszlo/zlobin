bca9e2132324b0082a42f5a96de3f6255a5b2846
makszlo <hlabn@yandex.ru>
2017-10-31 02:50:14 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,22 @@
+/**
+* Class Сalculator для части 001 урока 1
+*
+* @author Maxim Zlobin (mailto:hlabn@yandex.ru)
+* "since 31.10.2017
+* @version 0.1
+*/
 package ru.job4j;
 
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello world!");
+	}
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,25 @@
+/**
+* Test
+*
+* @author Maxim Zlobin (mailto:hlabn@yandex.ru)
+* "since 31.10.2017
+* @version 0.1
+*/
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculateTest{		
+	@Test
+	public void testMethod(){
+		String input="Max";
+		String expect="Echo, echo, echo : Max";
+		Calculate calc = new Calculate();
+		String result=calc.echo(input);
+		assertThat(result, is(expect));
+	}
+}
