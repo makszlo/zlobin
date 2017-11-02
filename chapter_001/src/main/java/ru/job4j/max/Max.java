5788f99e2dcd099226c4297f2bb4f0a2d37a6171
makszlo <hlabn@yandex.ru>
2017-11-02 23:54:47 +0300

    3.4. Максимум из трех чисел + комментарии

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -9,4 +9,7 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
+    public int max(int first, int second, int third) {
+        return this.max(first, this.max(second, third));
+    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -7,26 +7,46 @@ import static org.junit.Assert.assertThat;
 /**
  * Test class
  * @author Maxim Zlobin (mailto:hlabn@yandex.ru)
- * "since 02.11.2017
+ * @since 02.11.2017
  * @version 0.1
  */
 public class MaxTest {
+    /**
+     * Positive numbers test
+     */
     @Test
     public void positive() {
         Max testMax = new Max();
         int result = testMax.max(3, 9);
         assertThat(result, is(9));
     }
+
+    /**
+     * Negative numbers test
+     */
     @Test
     public void negative() {
         Max testMax = new Max();
         int result = testMax.max(-4, -23);
         assertThat(result, is(-4));
     }
+
+    /**
+     * Positive and negative numbers test
+     */
     @Test
     public void positiveAndNegative() {
         Max testMax = new Max();
         int result = testMax.max(-4, 32);
         assertThat(result, is(32));
     }
+    /**
+     * Three numbers test
+     */
+    public void threeNum() {
+        Max testMax = new Max();
+        int result = testMax.max(-43, 0, 112);
+        assertThat(result, is(112));
+    }
+
 }
