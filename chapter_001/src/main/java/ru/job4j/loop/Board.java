834235bae44136fc820cafa541ceff903f02fbdb
makszlo <hlabn@yandex.ru>
2017-11-08 21:15:44 +0300

    4.3. Построить шахматную доску в псевдографике.
    Board fix

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+/**
+ * Class for board task.
+ * @author Maxim Zlobin (mailto:hlabn@yandex.ru)
+ * @since 08.11.2017
+ * @version 0.1
+ */
+public class Board {
+    /**
+     * Рисует шахматную доску
+     * @param weight - ширина доски
+     * @param height - высота доски
+     * @return - String с шахматной доской
+     */
+    public String paint(int weight, int height) {
+        StringBuilder tmp = new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < weight; j++) {
+                if ((i + j) % 2 == 0) {
+                    tmp.append("X");
+                } else {
+                    tmp.append(" ");
+                }
+            }
+            tmp.append("\r\n");
+        }
+        return tmp.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test class for board task.
+ * @author Maxim Zlobin (mailto:hlabn@yandex.ru)
+ * @since 08.11.2017
+ * @version 0.1
+ */
+public class BoardTest {
+    /**
+     * Тест для доски 3х3
+     */
+    @Test
+    public void boardThreeXThree() {
+        Board brd = new Board();
+        String result = brd.paint(3, 3);
+        String newLine = System.getProperty("line.separator");
+        String expected = String.format("X X%s X %sX X%s", newLine, newLine, newLine);
+        assertThat(result, is(expected));
+    }
+    /**
+     * Тест для доски 5х4
+     */
+    @Test
+    public void boardFiveXFour() {
+        Board brd = new Board();
+        String result = brd.paint(5, 4);
+        String newLine = System.getProperty("line.separator");
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", newLine, newLine, newLine, newLine);
+        assertThat(result, is(expected));
+    }
+}
