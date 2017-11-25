46ba1a3e791a004556f9a7cc9bf0be74fa64bf12
makszlo <hlabn@yandex.ru>
2017-11-25 14:56:10 +0300

    Проверка, что одно слово находится в другом слове - правки

--- chapter_001/src/main/java/ru/job4j/ContainWord.java
+++ chapter_001/src/main/java/ru/job4j/ContainWord.java
@@ -3,29 +3,22 @@ package ru.job4j;
 public class ContainWord {
     public boolean contains(String origin, String sub) {
         int coincidenceCounter = 0;
-        int wordDiff = origin.length() - sub.length();
         char[] originCh = origin.toCharArray();
         char[] subCh = sub.toCharArray();
-        if (wordDiff >= 0) {
-            for (int i = 0; i <= wordDiff; i++) {
-                if (originCh[i] == subCh[0]) {
-                    for (int j = i; j < i + sub.length(); j++) {
-                        if (originCh[j] == subCh[j - i]) {
-                            coincidenceCounter++;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (coincidenceCounter == sub.length()) {
-                        return true;
+        int subLength = sub.length();
+        for (int i = 0; i <= origin.length() - subLength; i++) {
+            if ((originCh[i] == subCh[0]) && (coincidenceCounter != subLength)) {
+                coincidenceCounter = 1;
+                for (int j = i + 1; j < i + subLength; j++) {
+                    if (originCh[j] == subCh[j - i]) {
+                        coincidenceCounter++;
                     } else {
-                        coincidenceCounter = 0;
+                        break;
                     }
                 }
             }
-            return false;
-        } else {
-            return false;
         }
+        return coincidenceCounter == subLength;
     }
 }
+
