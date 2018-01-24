00555e997d66385fda86bf34fa9d3d6adc4e298c
makszlo <hlabn@yandex.ru>
2018-01-24 18:15:46 +0300

    Объеденение двух отсортированных массивов в один отсортированный массив - правки

--- chapter_001/src/main/java/ru/job4j/CombineTwoSortedInOneSorted.java
+++ chapter_001/src/main/java/ru/job4j/CombineTwoSortedInOneSorted.java
@@ -13,24 +13,22 @@ public class CombineTwoSortedInOneSorted {
      * @return - объеденённый отсортированный массив
      */
     public int[] combine(int[] a, int[] b) {
-        int lenghtA = a.length, lenghtB = b.length, lenght = lenghtA + lenghtB, counterA = 0, counterB = 0;
+        int lenghtA = a.length, lenghtB = b.length, lenght = lenghtA + lenghtB, counterA = 0, counterB = 0, i = 0;
         int[] result = new int[lenght];
-        for (int i = 0; i < lenght; i++) {
-            if (counterA < lenghtA && counterB < lenghtB) {
-                if (a[counterA] < b[counterB]) {
-                    result[i] = a[counterA];
-                    counterA++;
-                } else {
-                    result[i] = b[counterB];
-                    counterB++;
-                }
-            } else if (counterA < lenghtA && counterB == lenghtB) {
+        while (counterA < lenghtA && counterB < lenghtB) {
+            if (a[counterA] < b[counterB]) {
                 result[i] = a[counterA];
                 counterA++;
-            } else if (counterA == lenghtA && counterB < lenghtB) {
+            } else {
                 result[i] = b[counterB];
                 counterB++;
             }
+            i++;
+        }
+        if (counterB == lenghtB) {
+            System.arraycopy(a, counterA, result, i, lenghtA - counterA);
+        } else {
+            System.arraycopy(b, counterB, result, i, lenghtB - counterB);
         }
         return result;
     }
