120ad949f26d2d71b3f5dde1708d262b7c335af0
makszlo <hlabn@yandex.ru>
2018-06-04 19:32:07 +0300

    Кофемашина - правки

--- chapter_002/src/main/java/ru/job4j/change/Change.java
+++ chapter_002/src/main/java/ru/job4j/change/Change.java
@@ -17,38 +17,20 @@ public class Change {
      */
     public int[] change(int value, int price) {
         int[] change = {0, 0, 0, 0};
-        int tmp = value - price;
-        if (tmp > 0) {
-            while (tmp != 0) {
-                for (int i = 0; i < RANGE.length; i++) {
-                    while (tmp >= RANGE[i]) {
-                        change[i]++;
-                        tmp -= RANGE[i];
-                    }
-                }
-            }
-        }
-        return createArray(change);
-    }
-
-    /**
-     * Метод для создания массива монет
-     * @param change - массив с количеством монет разных типов
-     * @return - массив
-     */
-    private int[] createArray(int[] change) {
         int amount = 0;
-        for (int peace : change) {
-            amount += peace;
+        int tmp = value - price;
+        for (int i = 0; i < RANGE.length; i++) {
+                change[i] = tmp / RANGE[i];
+                tmp %=  RANGE[i];
+                amount += change[i];
         }
         int[] result = new int[amount];
         int i = 0;
-            for (int x = 0; x < RANGE.length; x++) {
-                for (int y = 0; y < change[x]; y++) {
-                    result[i] = RANGE[x];
-                    i++;
-                }
+        for (int x = 0; x < RANGE.length; x++) {
+            for (int y = 0; y < change[x]; y++) {
+                result[i++] = RANGE[x];
             }
+        }
         return result;
     }
 }
