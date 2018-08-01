37b14a1589951a0862ab10317aa60875da73999e
makszlo <hlabn@yandex.ru>
2018-08-01 20:55:19 +0300

    Комментарии и правки

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -2,15 +2,27 @@ package ru.job4j.list;
 
 import java.util.List;
 
+/**
+ * Класс для конвертации ArrayList в двухмерный массив
+ * @author Zlobin Maxim
+ */
 public class ConvertList2Array {
+    /**
+     * Метод конвертирующий ArrayList в двухмерный массив
+     * @param list - конвертируемый ArrayList
+     * @param rows - количество строк в двумерном массиве
+     * @return - двухмерный массив из ArrayList
+     */
     public int[][] toArray(List<Integer> list, int rows) {
         int ceils = (int) Math.ceil((double) list.size() / (double) rows);
         int[][] array = new int[rows][ceils];
         int counter = 0;
         for (int[] row : array) {
-            for(int i = 0; i < ceils; i++) {
+            for (int i = 0; i < ceils; i++) {
                 if (counter < list.size()) {
                     row[i] = list.get(counter++);
+                } else {
+                    row[i] = 0;
                 }
             }
         }
---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Тестовый класс для телефонного справочника
+ * @author Zlobin Maxim
+ * @version 1.0
+ */
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+}
